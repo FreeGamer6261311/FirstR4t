1be95df416254cd59b5ac556d550472bf0c70eb0
Mustafa Yazıcı <mstfyzc.29@gmail.com>
2025-03-30 12:27:38 +0300

    Eksik kısmı düzeltme

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require(‘express’);
-const cors = require(‘cors’);
+const express = require('express');
+const cors = require('cors');
 const app = express();
+
 app.use(cors());
-app.get(“/deneme”,function(req,res){
-res.send(“mkdir emirhankarakoc_rat_2023 && cd emirhankarakoc_rat_2023 && echo basarili > mesaj.txt”)
+
+app.get('/deneme', function(req, res) {
+  res.send('mkdir emirhankarakoc_rat_2023 && cd emirhankarakoc_rat_2023 && echo basarili > mesaj.txt');
 });
-const port = 2929
-app.listen(port);
-console.log(`Sunucu ${port} portunda calisiyor.`);
+
+const port = 2929;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda çalışıyor.`);
+});
